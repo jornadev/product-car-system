19e1f0290e175a2c04c84ec5a812fb38ee7eb438
Henrique Jornada <jornadahenrique12@gmail.com>
2025-01-07 11:05:49 -0300

    classe produto criada

--- src/bd/ConexaoBanco.java
+++ src/bd/ConexaoBanco.java
@@ -1,17 +1,17 @@
-package db;
+package bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class DatabaseConnection {
+public class ConexaoBanco {
 
     private static final String URL = "jdbc:mysql://localhost:3306/carrinho_virtual";
-    private static final String userName = "root";
-    private static final String password = "1203";
+    private static final String usuario = "root";
+    private static final String senha = "1203";
 
     public static Connection createConnectionToMySQL() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(URL, userName, password);
+        Connection connection = DriverManager.getConnection(URL, usuario, senha);
         return connection;
     }
 
