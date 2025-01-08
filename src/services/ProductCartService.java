44a9e6200fec16adf3fb939c8c0081f47b5213ff
Henrique Jornada <jornadahenrique12@gmail.com>
2025-01-08 16:36:02 -0300

    class ProductCartService created

---
+++ src/services/ProductCartService.java
@@ -0,0 +1,32 @@
+package services;
+
+import dao.ProductCartDAO;
+import models.Product;
+
+import java.util.List;
+
+public class ProductCartService {
+
+    private ProductCartDAO productCartDAO = new ProductCartDAO();
+
+    public void addProductToCart(Product product) throws Exception {
+        productCartDAO.insertProduct(product);
+    }
+
+    public void updateProductQuantity(int id, int newQuantity) throws Exception {
+        Product product = new Product();
+        product.setid(id);
+        product.setQuantity(newQuantity);
+        productCartDAO.editProductQuantity(product);
+    }
+
+    public void removeProductFromCart(int id) throws Exception {
+        Product product = new Product();
+        product.setid(id);
+        productCartDAO.removeProduct(product);
+    }
+
+    public double calculateTotalCartValue() throws Exception {
+        return productCartDAO.calculateTotalCartValue();
+    }
+}
