463d7d91fdbaa993df9c6343b81f7122d3f293a7
Henrique Jornada <jornadahenrique12@gmail.com>
2025-01-08 15:52:32 -0300

    class StockService created

--- src/services/StockService.java
+++ src/services/StockService.java
@@ -1,31 +1,30 @@
 package services;
 
-import dao.StockDAO;
+import dao.ProductCartDAO;
 import models.Product;
 import java.util.List;
 
-public class StockService {
+public class ProductCartService {
 
-    private StockDAO stockDAO = new StockDAO();
+    private ProductCartDAO productCartDAO = new ProductCartDAO();
 
-    public void addProductToStock(Product product) throws Exception {
-        stockDAO.insertProductOnStock(product);
+    public void addProductToCart(Product product) throws Exception {
+        productCartDAO.insertProduct(product);
     }
 
-    public void updateStockQuantity(int id, int newQuantity) throws Exception {
-        stockDAO.updateQuantity(id, newQuantity);
+    public void editProductQuantity(Product product) throws Exception {
+        productCartDAO.editProductQuantity(product);
     }
 
-    public void removeProductFromStock(int id) throws Exception {
-        stockDAO.removeProductOnStock(id);
+    public void removeProductFromCart(Product product) throws Exception {
+        productCartDAO.removeProduct(product);
     }
 
-    public double calculateTotalStockValue() throws Exception {
-        List<Product> products = stockDAO.listProductsOnStock();
-        double totalValue = 0.0;
-        for (Product product : products) {
-            totalValue += product.getPrice() * product.getQuantity();
-        }
-        return totalValue;
+    public double calculateTotalCartValue() throws Exception {
+        return productCartDAO.calculateTotalCartValue();
+    }
+
+    public List<Product> getProductsInCart() throws Exception {
+        return productCartDAO.getProductsFromCart();
     }
 }
